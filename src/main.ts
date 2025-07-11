5c73fb1fafad8f2fa4afc3fa40ea994facaf6c48
= <raj.lh404@gmail.com>
2025-07-11 11:23:00 +0530

    updated main ts

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,18 @@ async function bootstrap() {
     {
       transport: Transport.GRPC,
       options: {
-        url: '0.0.0.0:50544',
-        package: 'validate',
+        url: '0.0.0.0:50588',
+        package: 'check',
         protoPath: join(
           __dirname,
-          '../../common-modules/protocol/validate.proto',
+          '../../common-modules/protocol/check.proto',
         ),
       },
     },
   );
 
   await app.listen();
-  console.log('Validate Service is running on port 50544');
+  console.log('Validate Service is running on port 50588');
 }
 
 bootstrap();
